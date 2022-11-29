312903d042ff863272a1de621839c95b8f4da668
Perov Yuri <iury.perov@gmail.com>
2022-11-29 22:35:24 +0500

    Fixed 'or' operation in Boolean

--- Interpreter.Core/Execution/Objects/Boolean.cs
+++ Interpreter.Core/Execution/Objects/Boolean.cs
@@ -15,10 +15,10 @@ namespace Interpreter.Core.Execution.Objects
         
         public override Boolean ToBoolean() => new(Value);
 
-        public static Boolean operator !(Boolean operand) => new(!(bool)operand.Value);
+        public static Boolean operator !(Boolean operand) => new(!operand.IsTrue);
 
-        public static Boolean operator &(Boolean left, Boolean right) => new((bool)left.Value && (bool)right.Value);
+        public static Boolean operator &(Boolean left, Boolean right) => new(left.IsTrue && right.IsTrue);
         
-        public static Boolean operator |(Boolean left, Boolean right) => new((bool)left.Value && (bool)right.Value);
+        public static Boolean operator |(Boolean left, Boolean right) => new(left.IsTrue || right.IsTrue);
     }
 }
