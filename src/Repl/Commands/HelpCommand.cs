da4aac13dcca1b2dfda6e5506e9581f6892c655f
Perov Yuri <iury.perov@gmail.com>
2023-01-08 16:22:32 +0500

    Added PageUp and PageDown info in help message

--- src/Repl/Commands/HelpCommand.cs
+++ src/Repl/Commands/HelpCommand.cs
@@ -21,6 +21,7 @@ public class HelpCommand : ICommand
         Console.WriteLine(@"You can use:
 Enter to execute a submission.
 Arrows (↑ and ↓) to navigate within a multi-line submission.
+PageUp and PageDown to navigate through submission history.
 Backspace to remove the character on the left.");
         Console.WriteLine();
         
