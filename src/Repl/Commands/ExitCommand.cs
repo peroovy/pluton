7f96791015445d9548191498014b26d668a20065
Perov Yuri <iury.perov@gmail.com>
2023-01-08 12:15:31 +0500

    Implemented exit command

---
+++ src/Repl/Commands/ExitCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Repl.Commands;
+
+public class ExitCommand : ICommand
+{
+    public string Name => "exit";
+
+    public string Description => "Exit the REPL";
+    
+    public void Execute(string[] args)
+    {
+        Environment.Exit(0);
+    }
+}
