e61a6a6a188166160ca2b70cf82fa88183eee9d3
Perov Yuri <iury.perov@gmail.com>
2023-01-11 19:32:59 +0500

    Updated help message

--- src/Repl/MetaCommands/HelpCommand.cs
+++ src/Repl/MetaCommands/HelpCommand.cs
@@ -20,7 +20,7 @@ public class HelpCommand : IMetaCommand
     {
         Console.WriteLine(@"You can use:
 
-Enter to execute a submission.
+Enter to execute a submission or go to the next line.
 Arrows (↑ and ↓) to navigate within a multi-line submission.
 PageUp and PageDown to navigate through submission history.
 Backspace to remove the character on the left.
